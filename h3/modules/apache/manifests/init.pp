be0d79a27cc0a7a47d1897d53c9507432d020e45
Platypys <a1403025@myy.haaga-helia.fi>
2017-04-17 20:57:12 +0300

    templates and manifests

---
+++ h3/modules/apache/manifests/init.pp
@@ -0,0 +1,44 @@
+class apacheconf {
+	package{'apache2':
+		ensure => 'installed',
+	}
+
+	service{'apache2':
+		ensure => 'running',
+		enable => 'true',
+		require => Package['apache2'],
+	}
+	
+	file{'/home/joonas/public_html':
+		ensure => 'directory',
+		owner => 'joonas',
+		group => 'joonas',
+	}
+	
+	file{'/home/joonas/public_html/index.html':
+		ensure => 'file',
+		content => "Hello World",
+		owner => 'joonas',
+		group => 'joonas',
+	}
+
+	file{'/etc/apache2/sites-available/joonas.conf':
+		content => template('apacheconf/joonas.conf.erb'),
+		require => Package['apache2'],
+		owner => 'root',
+		group => 'root',
+	}
+	
+	file{'/etc/apache2/sites-enabled/joonas.conf':
+		ensure => 'link',
+		target => '/etc/apache2/sites-available/joonas.conf',
+		notify => Service['apache2'],
+		require => Package['apache2'],
+	}
+	
+	file{'/etc/apache2/sites-enabled/000-default.conf':
+		ensure => 'absent',
+		notify => Service['apache2'],
+		require => Package['apache2'],
+	}
+}
---
+++ h3/modules/ssh/manifests/init.pp
@@ -0,0 +1,17 @@
+class ssh{	
+	package{"ssh":
+		ensure => "installed",
+	}
+	
+	service{"sshd":
+		ensure => "running",
+		enable => "true",
+		require => Package["ssh"],
+	}
+	
+	file{"/etc/ssh/sshd_config":
+		content => template('ssh/sshd_config.erb'),
+		require => Package["ssh"],
+		notify => Service["sshd"],
+	}
+}
